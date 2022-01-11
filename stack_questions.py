4d073e47a39d2a57875f6782e32f360c174123c1
Sherzod Safarov <85355488+Sherzodd@users.noreply.github.com>
2022-01-11 20:51:19 +0500

    Fiinding questions without answers

--- stack_questions.py
+++ stack_questions.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow') #Stackoverflow questions api
 
-for data in response.json()['items']:
+for data in response.json()['items']:#Looping through json to find questions without answers
     if data['answer_count'] == 0:
         print(data['title'])
         print(data['link'])
