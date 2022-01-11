862bda01326db64d0dce96c60f83b7cba7ee88ee
Sherzod Safarov <85355488+Sherzodd@users.noreply.github.com>
2022-01-11 20:53:43 +0500

    Update stack_questions.py

--- stack_questions.py
+++ stack_questions.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow') #Stackoverflow questions api
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow') #Stackoverflow questions url
 
 for data in response.json()['items']:#Looping through json to find questions without answers
     if data['answer_count'] == 0:
